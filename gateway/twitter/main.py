af496d7f483a3b7547641f1b15cb23fe06839a08
Haruki Sao <haruki.sys@gmail.com>
2022-01-01 23:58:38 +0900

    [upd] Formatter: yapf で自動整形

--- gateway/twitter/main.py
+++ gateway/twitter/main.py
@@ -1,20 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+
 import requests
 import os
 
 app = FastAPI()
 
 # 環境設定
-URL=os.getenv('NEWSH_TWITTER_URL', None)
+URL = os.getenv('NEWSH_TWITTER_URL', None)
+
 
 class Trend(BaseModel):
     name: str
     tweet_volume: int = None
 
 
-@app.get("/twitter")
-async def call_twitter():
-    # Newsh Twitter API 呼び出し
+@app.get("/twitter/trends")
+async def call_twitter_trends():
+    # Newsh Twitter API (trends) 呼び出し
     response = requests.get(URL).json()
+
     return response
--- twitter/trends/main.py
+++ twitter/trends/main.py
@@ -10,23 +10,24 @@ from azure.identity import VisualStudioCodeCredential
 app = FastAPI()
 
 # 環境設定
-API_KEY=os.environ["TWITTER_API_KEY"]
-API_SECRET=os.environ["TWITTER_API_SECRET"]
-AT_KEY=os.environ["TWITTER_ACCESS_TOKEN_KEY"]
-AT_SECRET=os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+API_KEY = os.environ["TWITTER_API_KEY"]
+API_SECRET = os.environ["TWITTER_API_SECRET"]
+AT_KEY = os.environ["TWITTER_ACCESS_TOKEN_KEY"]
+AT_SECRET = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
 
 # ローカル実行時は Key Vault 参照機能不可
 if os.environ["Environment"] == "local":
     credential = VisualStudioCodeCredential()
-    client = SecretClient(vault_url="https://kv-newsh-test-je-001.vault.azure.net", credential=credential)
+    client = SecretClient(
+        vault_url="https://kv-newsh-test-je-001.vault.azure.net",
+        credential=credential)
     # シークレットを直接取得
     API_KEY = client.get_secret("TWITTER-API-KEY").value
     API_SECRET = client.get_secret("TWITTER-API-SECRET").value
     AT_KEY = client.get_secret("TWITTER-ACCESS-TOKEN-KEY").value
     AT_SECRET = client.get_secret("TWITTER-ACCESS-TOKEN-SECRET").value
 
-
-WID = 23424856 # 日本
+WID = 23424856  # 日本
 
 
 # モデルで必要な情報を絞り込み
