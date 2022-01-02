b3f7f5d57d421edcee3e09309f416c62c3285422
Haruki Sao <haruki.sys@gmail.com>
2022-01-02 18:53:30 +0900

    [upd] gateway をタイマートリガに変更

--- gateway/twitter/__init__.py
+++
@@ -1,9 +0,0 @@
-import azure.functions as func
-from bonnette import Bonnette
-
-from .main import app
-
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    handler = Bonnette(app)
-    return handler(req)
