57e2a4f057e8eefbd0c56bc0f07346f9c63a7dd1
Haruki Sao <haruki.sys@gmail.com>
2022-01-25 22:22:43 +0900

    [upd] UI Framework で MUI 導入
    参考）https://github.com/mui-org/material-ui/tree/master/examples/create-react-app-with-typescript

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,19 +1,34 @@
 import { VFC } from 'react'
-import logo from './logo.svg'
+// import logo from './logo.svg'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Link from '@mui/material/Link'
+import ProTip from './ProTip'
 import './App.css'
 
+const Copyright = () => (
+  <Typography variant="body2" color="text.secondary" align="center">
+    {'Copyright © Tech Meetup-Kinsol '}
+    {/* <Link color="inherit" href="https://mui.com/">
+      Tech Meetup-Kinsol
+    </Link>{' '} */}
+    {new Date().getFullYear()}.
+  </Typography>
+)
+
 const App: VFC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Container maxWidth="sm">
+    <Box sx={{ my: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        React App with TypeScript
+      </Typography>
+      <ProTip />
+      <Button variant="contained">Hello World</Button>
+      <Copyright />
+    </Box>
+  </Container>
 )
 
 export default App
