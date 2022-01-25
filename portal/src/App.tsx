0058c77b516ea8e7acee53ad66adb04e9216744a
Haruki Sao <haruki.sys@gmail.com>
2022-01-26 00:29:28 +0900

    [upd] ESLint 無視で Data Table を試してみる

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,11 +1,10 @@
 import { VFC } from 'react'
-// import logo from './logo.svg'
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Link from '@mui/material/Link'
-import ProTip from './ProTip'
 import './App.css'
 
 const Copyright = () => (
@@ -18,13 +17,52 @@ const Copyright = () => (
   </Typography>
 )
 
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.getValue(params.id, 'firstName') || ''} ${params.getValue(params.id, 'lastName') || ''}`,
+  },
+]
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+]
+
+const DataTable = () => (
+  <div style={{ height: 400, width: '100%' }}>
+    <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[5]} checkboxSelection />
+  </div>
+)
+
 const App: VFC = () => (
   <Container maxWidth="sm">
     <Box sx={{ my: 4 }}>
       <Typography variant="h4" component="h1" gutterBottom>
         React App with TypeScript
       </Typography>
-      <ProTip />
+      <DataTable />
       <Button variant="contained">Hello World</Button>
       <Copyright />
     </Box>
