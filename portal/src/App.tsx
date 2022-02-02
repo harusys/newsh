2485eded8861a898b97231724f0a8e416c703179
Haruki Sao <haruki.sys@gmail.com>
2022-02-03 01:16:21 +0900

    [upd] データ追加お試し実装

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,54 +1,13 @@
-import { VFC, useState, useEffect, forwardRef } from 'react'
-// import Avatar from 'react-avatar';
-// import Grid from '@material-ui/core/Grid'
-
-import MaterialTable, { Icons } from '@material-table/core'
-import AddBox from '@material-ui/icons/AddBox'
-import ArrowDownward from '@material-ui/icons/ArrowDownward'
-import Check from '@material-ui/icons/Check'
-import ChevronLeft from '@material-ui/icons/ChevronLeft'
-import ChevronRight from '@material-ui/icons/ChevronRight'
-import Clear from '@material-ui/icons/Clear'
-import DeleteOutline from '@material-ui/icons/DeleteOutline'
-import Edit from '@material-ui/icons/Edit'
-import FilterList from '@material-ui/icons/FilterList'
-import FirstPage from '@material-ui/icons/FirstPage'
-import LastPage from '@material-ui/icons/LastPage'
-import Remove from '@material-ui/icons/Remove'
-import SaveAlt from '@material-ui/icons/SaveAlt'
-import Search from '@material-ui/icons/Search'
-import ViewColumn from '@material-ui/icons/ViewColumn'
+import { VFC, useState, useEffect } from 'react'
+import MaterialTable from '@material-table/core'
 import axios from 'axios'
-
-const tableIcons: Icons = {
-  /* eslint-disable react/jsx-props-no-spreading */
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-  /* eslint-enable react/jsx-props-no-spreading */
-}
+import tableIcons from './MaterialTableIcons'
 
 const api = axios.create({
   responseType: 'json',
 })
 
-export type ModelItem = Pick<Model, 'id' | 'user_id' | 'task_name' | 'scheduled_at'>
-
-export interface Model {
+interface ITimerManager {
   id: string
   user_id: string
   task_name: string
@@ -56,9 +15,6 @@ export interface Model {
 }
 
 const App: VFC = () => {
-  //   type IType = 'string' | 'boolean' | 'numeric' | 'date' | 'datetime' | 'time' | 'currency'
-  //   const string: IType = 'string'
-
   const [columns, setColumns] = useState([
     { title: 'ID', field: 'id' },
     { title: 'ユーザID', field: 'user_id', hidden: true },
@@ -66,7 +22,7 @@ const App: VFC = () => {
     { title: '通知時刻', field: 'scheduled_at' },
   ])
 
-  const [data, setData] = useState<Array<ModelItem>>([])
+  const [data, setData] = useState<Array<ITimerManager>>([])
 
   // for error handling
   const [iserror, setIserror] = useState(false)
@@ -74,10 +30,10 @@ const App: VFC = () => {
 
   useEffect(() => {
     api
-      .get<Array<Model>>('/api/timer-manager/Ud9f705bf1ae17b6111b1b5353b00eaf7')
+      .get<Array<ITimerManager>>('/api/timer-manager/Ud9f705bf1ae17b6111b1b5353b00eaf7')
       .then((response) => {
         setData(
-          response.data.map<ModelItem>((d) => ({
+          response.data.map<ITimerManager>((d) => ({
             id: d.id,
             user_id: d.user_id,
             task_name: d.task_name,
@@ -93,7 +49,7 @@ const App: VFC = () => {
       })
   }, [])
 
-  const handleRowUpdate = (newData: ModelItem, oldData: ModelItem | undefined) => {
+  const handleRowUpdate = (newData: ITimerManager, oldData: ITimerManager | undefined) => {
     // validation
     const errorList = []
     if (newData.task_name === '') {
@@ -106,13 +62,13 @@ const App: VFC = () => {
     if (errorList.length < 1) {
       api
         .patch(`/api/timer-manager/${newData.id}`, newData)
-        .then((response) => {
+        .then((res) => {
           //   const dataUpdate = [...data]
           //   const index = oldData.id
           //   dataUpdate[index] = newData
           //   setData([...dataUpdate])
-          //   setIserror(false)
-          //   setErrorMessages([])
+          setErrorMessages([])
+          setIserror(false)
         })
         .catch((e: unknown) => {
           if (e instanceof Error) {
@@ -126,8 +82,12 @@ const App: VFC = () => {
     }
   }
 
-  const handleRowAdd = (newData: ModelItem) => {
+  const handleRowAdd = (newData: ITimerManager) => {
     // 検証
+    /* eslint-disable */
+    newData.user_id = 'xxx'
+    console.log(newData)
+    /* eslint-enable */
     const errorList = []
     if (newData.task_name === undefined) {
       errorList.push('コンテンツ名を入力してください')
@@ -140,9 +100,7 @@ const App: VFC = () => {
       api
         .post('/api/timer-manager', newData)
         .then((res) => {
-          const dataToAdd = [...data]
-          dataToAdd.push(newData)
-          setData(dataToAdd)
+          setData([...data, newData])
           setErrorMessages([])
           setIserror(false)
         })
@@ -160,22 +118,21 @@ const App: VFC = () => {
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-
       <MaterialTable
-        title="Editable Preview"
+        title="通知設定画面"
         columns={columns}
         data={data}
         icons={tableIcons}
         editable={{
-          onRowAdd: (newData: ModelItem) =>
+          onRowAdd: (newData: ITimerManager) =>
             new Promise((resolve) => {
               handleRowAdd(newData)
+              resolve('OK')
             }),
-          onRowUpdate: (newData: ModelItem, oldData: ModelItem | undefined) =>
+          onRowUpdate: (newData: ITimerManager, oldData: ITimerManager | undefined) =>
             new Promise((resolve) => {
               handleRowUpdate(newData, oldData)
+              resolve('OK')
             }),
           //   onRowDelete: (oldData: Model) =>
           //     new Promise((resolve) => {
