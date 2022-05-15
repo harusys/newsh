c0c52612e3edbe32350fa35130d4fee0912bc161
kawai <n.hrtk7623@gmail.com>
2022-05-16 00:18:55 +0900

    weatherモデルの定義修正

--- weather/weather/main.py
+++ weather/weather/main.py
@@ -1,12 +1,17 @@
 import json
 import logging
 import os
+
+# from datetime import date
 from typing import List
 
 import requests
-from azure.identity import VisualStudioCodeCredential
-from azure.keyvault.secrets import SecretClient
+
+# from azure.identity import VisualStudioCodeCredential
+# from azure.keyvault.secrets import SecretClient
 from fastapi import FastAPI
+
+# from gateway.timer import main
 from pydantic import BaseModel, parse_obj_as
 
 app = FastAPI()
@@ -19,61 +24,88 @@ city = os.environ["CITY_NAME"]
 # city = event.message.text
 
 # ローカル実行時は Key Vault 参照機能不可
-if os.environ["Environment"] == "local":
-    credential = VisualStudioCodeCredential()
-    client = SecretClient(
-        vault_url="https://kv-newsh-test-je-001.vault.azure.net",
-        credential=credential,
-    )
-    # シークレットを直接取得
-    API_KEY = client.get_secret("OPEN-WEATHER-API-KEY").value
+# if os.environ["Environment"] == "local":
+#     credential = VisualStudioCodeCredential()
+#     client = SecretClient(
+#         vault_url="https://kv-newsh-test-je-001.vault.azure.net",
+#         credential=credential,
+#     )
+#     # シークレットを直接取得
+#     API_KEY = client.get_secret("OPEN-WEATHER-API-KEY").value
 
 
 # モデルで必要な情報を絞り込み
+
+
+class WeatherList(BaseModel):
+    id: int
+    main: str
+    description: str
+    icon: str
+
+
+class MainList(BaseModel):
+    temp: float
+    feels_like: float
+    temp_min: float
+    temp_max: float
+    pressure: int
+    sea_level: int
+    grnd_level: int
+    humidity: int
+    temp_kf: float
+
+
+# class RainList(BaseModel):
+#     3h: float
+
+
 class Weather(BaseModel):
-    weather_description: str
-    temperature: float
-    rainfall: float
+    dt: int
+    weather: List[WeatherList]
+    main: MainList
+    visibility: int
+    wind: dict
+    clouds: dict
+    dt: int
+    sys: dict
+    dt_txt: str
+    rain: dict = None
+    pop: int
 
 
-class WeatherGet2(BaseModel):
-    weather_description: str
-    temperature: float
-    rainfall: float
+class WeatherResponse(BaseModel):
+    WeatherDescription: str
+    Temperature: float
+    MaxTemperature: float
+    MinTemperature: float
 
 
-class WeatherGet(BaseModel):
-    weather_description: WeatherGet2
-    temperature: float
-    rainfall: float
-
-
-@app.get("/weather", response_model=List[Weather])
+@app.get("/weather", response_model=List[WeatherResponse])
 async def weather_get():
 
     # 天気情報を取得
     response = requests.get(
         f"{BASE_URL}?q={city}&units=metric&lang=ja&APPID={API_KEY}"
     )
-    forecastData = json.loads(response.text)
+    forecastData = json.loads(response.text)["list"]
     forecastData_items = parse_obj_as(List[Weather], forecastData)
 
     # TODO: 複数の時間での天気情報取得
-    weatherDescription = forecastData_items[-1].weather_description
-    temperature = forecastData_items[-1]["main"]["temp"]
-    rainfall = 0
-    if (
-        "rain" in forecastData_items[-1]
-        and "3h" in forecastData_items[-1]["rain"]
-    ):
-        rainfall = forecastData_items[-1]["rain"]["3h"]
+    weatherDescription = forecastData_items[-1].weather[-1].description
+    temperature_now = forecastData_items[-1].main.temp
+    temperature_max = forecastData_items[-1].main.temp_max
+    temperature_min = forecastData_items[-1].main.temp_min
 
-    weatherList = {}
-    weatherList["WeatherDescription"] = weatherDescription
-    weatherList["Temperature"] = temperature
-    weatherList["Rainfall"] = rainfall
+    weatherDict = {}
+    weatherDict["WeatherDescription"] = weatherDescription
+    weatherDict["Temperature"] = temperature_now
+    weatherDict["MaxTemperature"] = temperature_max
+    weatherDict["MinTemperature"] = temperature_min
 
-    logging.info(f"Return of main is{weatherList}")
+    weatherList = []
+    weatherList.append(weatherDict)
+    logging.info(f"#############################{weatherList}")
 
     # 応答
     return weatherList
